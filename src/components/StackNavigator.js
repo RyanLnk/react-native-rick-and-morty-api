a4eaf67e871140ef08aa0b0d0425b30d068c5819
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2022-01-19 11:03:51 -0300

    Criação da navegação no app

---
+++ src/components/StackNavigator.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import tw from 'twrnc';
+import CharacterListScreen from '../screens/CharacterListScreen';
+import CharacterScreen from '../screens/CharacterScreen';
+import LocationScreen from '../screens/LocationScreen';
+import EpisodeScreen from '../screens/EpisodeScreen';
+
+const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false, contentStyle: tw`bg-gray-900 p-3` }}
+    >
+      <Stack.Screen
+        name="CharacterListScreen"
+        component={CharacterListScreen}
+      />
+      <Stack.Screen name="CharacterScreen" component={CharacterScreen} />
+      <Stack.Screen name="LocationScreen" component={LocationScreen} />
+      <Stack.Screen name="EpisodeScreen" component={EpisodeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
